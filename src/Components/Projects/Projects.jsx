cc84a3850b46f29fb0f272716c0d4a5d10dce2fe
Nimesh <nimeshkayastha067@gmail.com>
2024-11-23 16:12:27 +0545

    fix

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
     <div id='Projects' className='p-10 md:p-12 text-white'>
         <h1 className='text-2xl md:text-4xl text-white font-bold text-center'>Projects</h1>
         <div className='py-12 px-8 flex flex-wrap gap-7 mx-20'>
-            <ProjectCard title="Rock Paper Scissor " main="This is a Rock Paper Scissorweb game website created using js" Link=" https://nimesh631.github.io/rockpaperscissor/"/>
+            <ProjectCard title="Rock Paper Scissor " main="This is a Rock Paper Scissor web game website created using js" Link=" https://nimesh631.github.io/rockpaperscissor/"/>
             <ProjectCard title="Quiz" main="This is a Quiz game website created using js" Link="https://nimesh631.github.io/Quiz/"/>
             {/* <ProjectCard title="Todo App" main="This is a Todo App  created using react-redux"/> */}
         </div>

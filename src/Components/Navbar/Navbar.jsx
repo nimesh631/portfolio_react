bf65ec64a13dccc3f1370cb8933bf9170c473bab
nimesh631 <nimeshkayastha067@gmail.com>
2025-04-25 12:15:31 +0545

    style: margin top increased

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,10 +12,10 @@ function Navbar() {
 
     return (
         <nav className='fixed top-0 left-0 z-50 w-full bg-blue-950 rounded-lg flex flex-wrap justify-between h-15 md:items-center text-white px-10 py-1 md:px-20'>
-            <span className='text-xl font-bold tracking-wide'>Portfolio</span>
-            <ul className={`${menu ? "block" : "hidden"} mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6 `}>
+            <span className='text-xl font-bold tracking-wide mt-4'>Portfolio</span>
+            <ul className={`${menu ? "block" : "hidden"} mx-24 py-1 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}>
                 <a href="#About">
-                    <li className='text-md transition-all duration-300 p-1 md:p-0 hover:text-blue-400'>About</li>
+                    <li className='text-md transition-all duration-300 p-2 md:p-0 hover:text-blue-400'>About</li>
                 </a>
 
                 <a href="#Skills">

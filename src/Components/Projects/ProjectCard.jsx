166edc949844b18e1c64a78075b3efa3944f802a
nimesh631 <nimeshkayastha067@gmail.com>
2025-09-14 20:38:44 +0545

    feat: add new projects

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -8,15 +8,14 @@ function ProjectCard({title,main,Link,text}) {
         <h3 className='px4 text-xl md:text-2xl font-bold leading-normal text-center'>
             {title}
         </h3>
-      <p className='px-4 text-sm md:tex-md leading-tight py-2'>{main}</p>
+      <p className='px-4 text-sm md:text-md leading-tight py-2 flex-grow'>{main}</p>
       <div className='mt-2 p-2 md:p-4 flex gap-2 md:gap-4 justify-center'>
-        <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697] '>
-            <a href={Link}>{text}</a>
-        </button>
-
-        {/* <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>
-            Source code
-        </button> */}
+      <a
+  href={Link}
+  className='text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697] text-center'
+>
+  {text}
+</a>
       </div>
     </div>
   )
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,22 +1,77 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
+import React from "react";
+import ProjectCard from "./ProjectCard";
 
 function Projects() {
   return (
-    <div id='Projects' className='p-10 md:p-12 text-white'>
-        <h1 className='text-2xl md:text-4xl text-white font-bold text-center mt-10'>Projects</h1>
-        <div className='py-12 px-8 flex flex-wrap gap-7 mx-20'>
-          <ProjectCard title="Todo" text="Demo" main="Feature-rich Todo App built with React and Redux Toolkit that offers complete CRUD operations and data persistence via Local Storage. Streamlined task management with an intuitive interface." Link="https://nimesh631.github.io/ReduxToolKitTodo/"/>
-            <ProjectCard title="Expense Tracker" text="Demo" main="A simple and responsive expense tracking web app built with React, Redux Toolkit, and Vite. Users can add income and expense transactions, view real-time balance updates, and track financial summaries." Link="https://nimesh631.github.io/reduxtoolkitExpenseTracker/"/>
-            <ProjectCard title="Quiz" text="Demo" main="A dynamic Quiz Game website built with JavaScript that implements event handling for interactive user experience, tracks and displays user scores, and utilizes query selectors for efficient DOM manipulation and content updates" Link="https://nimesh631.github.io/Quiz/"/>
-            <ProjectCard title="Currency" text="Demo" main="This is a Currency Convertor App created using react. It makes use of Reusable components" Link="https://nimesh631.github.io/chai-aur-react/"/>
-            <ProjectCard title="Fake API Frontend" text="Repo" main="This is a Fake API having responsive product list page with pagination, dynamic routing.
-" Link="https://github.com/nimesh631/TaskProductApi"/>
- <ProjectCard title="Job Portal Backend" text="Repo" main="This is a comprehensive job portal and crowdfunding backend using Nest.js, TypeORM, PostgreSQL, and TypeScript. The system features CRUD operations, JWT authentication, and role-based authorization.
-" Link="https://github.com/nimesh631/JobPortal"/>
-        </div>
+    <div id="Projects" className="p-10 md:p-12 text-white">
+      <h1 className="text-2xl md:text-4xl text-white font-bold text-center mt-10">
+        Projects
+      </h1>
+    <div className="py-12 px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-30 gap-y-5"> 
+        <ProjectCard
+          title="Fullstack Expense Tracker"
+          text="Repo"
+          main="Built a full-stack Expense Tracker with NestJS, TypeORM, and PostgreSQL, featuring JWT authentication and DTO validation. Developed a React (TypeScript) frontend with TailwindCSS for expense CRUD operations. Implemented secure API integration and toast-based user feedback."
+          Link="https://github.com/nimesh631/ExpenseTrackerFullStack"
+        />
+        <ProjectCard
+          title="Fullstack Task Management App"
+          text="Repo"
+          main="React + NestJS CRUD with JWT auth and PostgreSQL.Task tracker with secure login and REST APIs.
+.Responsive UI with authentication and database integration.
+"
+          Link="https://github.com/nimesh631/TaskTodoFullStack"
+        />
+        <ProjectCard
+          title="Job Portal Backend"
+          text="Repo"
+          main="Built with NestJS, TypeORM, PostgreSQL, and TypeScript, CRUD, JWT authentication, and role-based authorization.
+"
+          Link="https://github.com/nimesh631/JobPortal"
+        />
+
+        <ProjectCard
+          title="Todo"
+          text="Demo"
+          main="Feature-rich Todo App built with React and Redux Toolkit that offers complete CRUD operations and data persistence via Local Storage. Streamlined task management with an intuitive interface."
+          Link="https://nimesh631.github.io/ReduxToolKitTodo/"
+        />
+        <ProjectCard
+          title="Expense Tracker"
+          text="Demo"
+          main="A simple and responsive expense tracking web app built with React, Redux Toolkit, and Vite. Users can add income and expense transactions, view real-time balance updates, and track financial summaries."
+          Link="https://nimesh631.github.io/reduxtoolkitExpenseTracker/"
+        />
+        <ProjectCard
+          title="Fake API Frontend"
+          text="Repo"
+          main="This is a Fake API having responsive product list page with pagination, dynamic routing.
+"
+          Link="https://github.com/nimesh631/TaskProductApi"
+        />
+
+            <ProjectCard
+          title="Live User Search"
+          text="Repo"
+          main="Developed a responsive project implementing live user search with debounce to optimize API calls and improve performance, display user data efficiently, providing smooth and real-time search experience.
+"
+          Link=" https://github.com/nimesh631/LiveUserSearchDebounce"
+        />
+        <ProjectCard
+          title="Currency"
+          text="Demo"
+          main="This is a Currency Convertor App created using react. It makes use of Reusable components"
+          Link="https://nimesh631.github.io/chai-aur-react/"
+        />
+        <ProjectCard
+          title="Quiz"
+          text="Demo"
+          main="A dynamic Quiz Game website built with JavaScript, event handling for interactive UX, tracks and displays user scores, and utilizes query selectors for efficient DOM manipulation and content updates"
+          Link="https://nimesh631.github.io/Quiz/"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
